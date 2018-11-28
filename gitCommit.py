49044a1c91cacd3abf705dd94486bb8e6c4a3697
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-11-28 13:50:34 -0500

    change them to display only images by default

--- gitCommit.py
+++ gitCommit.py
@@ -3,5 +3,4 @@ import subprocess
 commitMsg = input()
 subprocess.run(['git','add','.'])
 subprocess.run(['git','commit','-m',commitMsg])
-subprocess.run(['git','push'])
-subprocess.run(['git', 'status'])
+subprocess.run(['git','push', 'origin', 'jsrollover'])
